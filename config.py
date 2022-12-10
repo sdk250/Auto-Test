cbb2b5bffa88e0f80fd51176a07f49557e845048
sdk250 <520sdk250@gmail.com>
2022-12-10 21:50:08 +0800

    优化多账户管理逻辑

--- config.py
+++ config.py
@@ -11,14 +11,47 @@ else:
 	quit()
 
 ID = {
-	"Account": "Password" # 多账号支持
+	"Account": {
+		"longitude": "Your longitude",
+		"latitude": "Your latitude",
+		"address": "Your address for text",
+		"info": {
+			"returnSchool": "是",
+			"temperature": str(round((36 + uniform(0, 0.9)), 1)),
+			"state": ["以上都无"],
+			"mood": "好"
+		},
+		"email_server": False, # 如果设置为True，则必须填写下面三项
+		# "server_email": "Send email",
+		# "server_email_key": "Send email key",
+		"client_email": "Recv email",
+		# 取消注释则优先使用
+		"passwd": "Password" # 多账号支持
+	},
+	"Account2": {
+		"longitude": "Your longitude2",
+		"latitude": "Your latitude2",
+		"address": "Your address for text2",
+		"info": {
+			"returnSchool": "是",
+			"temperature": str(round((36 + uniform(0, 0.9)), 1)),
+			"state": ["以上都无"],
+			"mood": "好"
+		},
+		"email_server": False, # 如果设置为True，则必须填写下面三项
+		"server_email": "Send email",
+		# "server_email_key": "Send email key",
+		"client_email": "Recv email2",
+		# 取消注释则优先使用
+		"passwd": "Password2" # 多账号支持
+	}
 }
 
 global_longitude = "Your global longitude"
 global_latitude = "Your global latitude"
 global_address = "Your address for text"
 
-info = {
+global_info = {
 	"returnSchool": "是", # 是否返校
 	"temperature": str(round((36 + uniform(0, 0.9)), 1)), # 随机体温值
 	"state": ["以上都无"], # 您当前的状态
@@ -37,7 +70,7 @@ global_headers = {
 	"Connection": "Close"
 }
 
-email_server = False # 如果设置为True，则必须填写下面三项
-server_email = "Send email"
-server_email_key = "Send email key"
-client_email = "Recv email"
+global_email_server = False # 如果设置为True，则必须填写下面三项
+global_server_email = "GLOBAL Send email"
+global_server_email_key = "GLOBAL Send email key"
+global_client_email = "GLOBAL Recv email"
